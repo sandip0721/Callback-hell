529a0f682e433426a5f290d2b874007e40900f7c
sandip0721 <75519490+sandip0721@users.noreply.github.com>
2021-04-18 09:53:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(()=>{
+  document.getElementById('div').innerHTML=10
+  setTimeout(()=>{
+      document.getElementById('div').innerHTML=9
+      setTimeout(()=>{
+          document.getElementById('div').innerHTML=8
+          setTimeout(()=>{
+              document.getElementById('div').innerHTML=7
+              setTimeout(()=>{
+                  document.getElementById('div').innerHTML=6
+                  setTimeout(()=>{
+                      document.getElementById('div').innerHTML=5
+                      setTimeout(()=>{
+                          document.getElementById('div').innerHTML=4
+                          setTimeout(()=>{
+                              document.getElementById('div').innerHTML=3
+                              setTimeout(()=>{
+                                  document.getElementById('div').innerHTML=2
+                                  setTimeout(()=>{
+                                      document.getElementById('div').innerHTML=1
+                                      setTimeout(()=>{
+                                          document.getElementById('div').innerHTML="Happy Independence Day"
+                                      },1000)    
+                                  },1000);
+                              },1000);
+                          },1000);
+                      },1000);
+                  },1000);
+              },1000);
+          },1000);
+      },1000);
+  },1000); 
+},1000);                               
